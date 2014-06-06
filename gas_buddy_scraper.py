21315137f5062194103101a5943a2d440c830d48
Michael Ramm <micramm@gmail.com>
2014-06-05 21:20:26 -0700

    adding scrapping

--- gas_buddy_scraper.py
+++ gas_buddy_scraper.py
@@ -1,23 +1,46 @@
-from access_keys import access_keys
+import requests
+from pattern import web
+import time
+import os
 
+download_delay = 0
 
+information_url = 'http://www.sanfrangasprices.com/GasPriceSearch.aspx?typ=adv&fuel=A&srch=0&area=All%20Areas'
 
-class google_api(object):
-    
-    def __init__(self):
-        self.key = access_keys.google_key
+html = requests.get(information_url).text
+dom = web.Element(html)
 
-    def generate_url(self, addresses):
-        base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}W&destinations={1}&sensor=false&key={2}'
-        rows = '|'.join(addresses)
-        url = base_url.format(rows, rows, self.key)
-        print url
-    
-api = google_api()
-addresses = ['7 Avocet Dr. Redwood City, CA, 94065',
-             '640 Masonic Way, Belmont, CA 94002',
-             '716 Laurel St San Carlos, CA 94070',
-             '796 Laurel St San Carlos, CA 94070',
-             '260 Sheridan Ave, Palo Alto, CA',
-             ]
-api.generate_url(addresses * 2)
+#get a list of all station brands
+gas_stations = set()
+for element in dom.by_tag('td'):
+    table = element.by_id('ctl00_Content_P_PSC1_lstStations')
+    if table:
+        for entry in table.by_tag('option'):
+            gas_stations.add(entry.content)
+        break
+gas_stations = gas_stations - set([u'All Stations'])
+#get a list of all areas:
+areas = set()
+for element in dom.by_tag('td'):
+    table = element.by_id('ctl00_Content_P_PSC1_lstAreas')
+    if table:
+        for entry in table.by_tag('option'):
+            areas.add(entry.content)
+        break
+areas = areas - set([u'All Areas'])
+#download all of the html
+base_download_url = 'http://www.sanfrangasprices.com/GasPriceSearch.aspx?typ=adv&fuel=A&srch=0&area={0}&site=SanFran&station={1}&tme_limit=36'
+
+dir_append = time.strftime('%b-%d-%Y-%H-%M-%S', time.localtime())
+directory = 'gas_buddy_data/html/{}'.format(dir_append)
+os.mkdir(directory)
+os.chdir(directory)
+for area in areas:
+    for gas_station in gas_stations:
+        print 'Download: {}, {}'.format(area, gas_station)
+        download_url =  base_download_url.format(area, gas_station)
+        html = requests.get(information_url).text
+        current_file = open("{}-{}.html".format(area, gas_station),"w")
+        current_file.write(html)
+        current_file.close()
+        time.sleep(download_delay)
--- yelp_scrapper.py
+++ yelp_scrapper.py
@@ -3,6 +3,8 @@ import oauth2
 import urllib
 import urllib2
 from access_keys import access_keys
+import os
+import time
 
 consumer_key = access_keys.yelp_consumer_key
 consumer_secret = access_keys.yelp_consumer_secret
@@ -13,7 +15,7 @@ def process_request(offset, category, bounds='37.9736,-122.5311|37.3333,-121.900
     url_params = {}
     url_params['bounds'] = bounds 
     url_params['offset'] = offset
-    url_params['category_filter'] = 'drugstores'
+    url_params['category_filter'] = category
     response = request('api.yelp.com','/v2/search', url_params, consumer_key, consumer_secret, token, token_secret)
     return response
 
@@ -45,23 +47,77 @@ def request(host, path, url_params, consumer_key, consumer_secret, token, token_
         response = json.loads(error.read())    
     return response
 
-#banks
-#drugstores
-#postoffices
-#restaurants
-#food
+def get_total_result(category, bounds):
+    '''
+    returns the number of total results for the category inside bounds
+    '''
+    totals = []
+    for bound in bounds:
+        response = process_request(0, category, bound)
+        totals.append(response['total'])
+    print totals
+    return max(totals)
 
-response = process_request(0, 'banks')
-print json.dumps(response, sort_keys=True, indent = 4)
-# print response['businesses'][0].keys()
+def division_to_bounds(division, map_bounds):
+    lat_start, lon_start, lat_stop, lon_stop = map_bounds
+    delta = (lon_stop - lon_start) / division
+    bound_form = '{:.4f},{:.4f}|{:.4f},{:.4f}'
+    bounds = [bound_form.format(lat_start, lon_start + i * delta, lat_stop, lon_start + (i + 1) * delta) for i in range(division)]
+    return bounds
 
-try:
-    names = []
-    for b in response['businesses']:
-        names.append(b['name'])
-#         print b['rating']
-        print b['display_address']
-    print len(response['businesses'])
-    print sorted(names)
-except Exception as e:
-    print response
+def get_required_division(category, max_elements = 1000, map_bounds=(37.9736,-122.5311,37.3333,-121.9000)):
+    '''
+    returns the number of segments the latitude needs to be split into such that
+    the number of businesses in every segment is below max_elements
+    '''
+    division = 1
+    bounds = division_to_bounds(division, map_bounds)
+    while get_total_result(category, bounds) > max_elements:
+        division *= 2
+        bounds = division_to_bounds(division, map_bounds)
+    return division
+
+def save_one_bound(category, bound, division_count):
+    start = 0
+    response = process_request(start, category, bound)
+    result_count = len(response['businesses'])
+    while result_count:
+        text =  json.dumps(response, sort_keys=True)
+        with open('data_{}_{}.txt'.format(division_count, start), 'w') as outfile:
+            json.dump(text, outfile)
+        start += 20
+        response = process_request(start, category, bound)
+        result_count = len(response['businesses'])
+        
+def save_all_bounds(category, required_divisions, map_bounds=(37.9736,-122.5311,37.3333,-121.9000)):
+    dir_append = time.strftime('%b-%d-%Y-%H-%M-%S', time.localtime())
+    directory = 'yelp_data/html/{}/{}'.format(category, dir_append)
+    os.mkdir(directory)
+    os.chdir(directory)
+    bounds = division_to_bounds(required_divisions, map_bounds)
+    for count, bound in enumerate(bounds):
+        print 'saving division', count
+        save_one_bound(category, bound, count)
+    
+
+# print get_required_division('food')
+# save_all_bounds('banks', 2)
+# save_all_bounds('drugstores', 1)
+# save_all_bounds('postoffices', 1)
+# save_all_bounds('restaurants', 128)
+save_all_bounds('food', 64)
+
+# response = process_request(0, 'banks')
+# print json.dumps(response, sort_keys=True, indent = 4)
+# # print response['businesses'][0].keys()
+# 
+# try:
+#     names = []
+#     for b in response['businesses']:
+#         names.append(b['name'])
+# #         print b['rating']
+#         print b['display_address']
+#     print len(response['businesses'])
+#     print sorted(names)
+# except Exception as e:
+#     print response
